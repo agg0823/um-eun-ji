614539416d17dfd5e3a450cf73904edd1477f5ff
‘agg0823’ <lui0508928@gmail.com>
2022-04-27 13:54:09 +0900

    index 수정

--- js/common.js
+++ js/common.js
@@ -24,14 +24,15 @@ var swiper = new Swiper(".blog-slider", {
   spaceBetween: 30,
   effect: "fade",
   loop: true,
-  mousewheel: {
-    invert: false
-  },
+  direction: 'horizontal',
+  autoplay : true,
+
   // autoHeight: true,
   pagination: {
     el: ".blog-slider__pagination",
     clickable: true
   }
+  
 });
 
 
